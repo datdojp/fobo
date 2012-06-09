c8e52f1be8c6dadc81e98fd3c450423f99f29019
datdvt@gmail.com <datdvt@gmail.com@406429b8-78a3-c801-e1f8-7778f066512d>
2012-06-09 09:07:33 +0000

    create new project

---
+++ trunk/Fobo/src/com/fobo/rss/FoboActivity.java
@@ -0,0 +1,13 @@
+package com.fobo.rss;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FoboActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
